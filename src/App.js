245960bbab25319a61b27a9a4f7f3c79ea543c3b
tsiq-swyx <shawn.wang@twosigmaiq.com>
2018-07-12 15:41:29 -0400

    test adding

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
+import Textarea from 'react-textarea-autosize';
 import logo from './logo.svg';
 import './App.css';
 
 class LambdaDemo extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { loading: false, msg: null };
-	}
+	state = { raw: 'please type some stuff', text: '' };
 
-	handleClick = e => {
-		e.preventDefault();
-
-		this.setState({ loading: true });
+	handleRaw = e => {
+		this.setState(() => ({ raw: e.target.value }));
+	};
+	convertRaw = e => {
 		fetch('/.netlify/functions/hello', {
 			method: 'POST', // *GET, POST, PUT, DELETE, etc.
 			// mode: "cors", // no-cors, cors, *same-origin
@@ -23,20 +21,48 @@ class LambdaDemo extends Component {
 			},
 			// redirect: "follow", // manual, *follow, error
 			// referrer: "no-referrer", // no-referrer, *client
-			body: JSON.stringify({ test: 1 }) // body data type must match "Content-Type" header
-		})
-			.then(response => response.json())
-			.then(json => this.setState({ loading: false, msg: json.msg }));
+			body: JSON.stringify(this.state.raw) // body data type must match "Content-Type" header
+		}).then(response => console.log(response)); // response.json())
+		// .then(json => this.setState({ test: false, msg: json.msg }));
+		// this.setState(() => ({ text: processContent(e.target.value) }));
 	};
 
+	// handleClick = e => {
+	// 	e.preventDefault();
+
+	// 	this.setState({ loading: true });
+	// };
+
 	render() {
-		const { loading, msg } = this.state;
+		const { text, raw } = this.state;
 
 		return (
 			<p>
-				<button onClick={this.handleClick}>{loading ? 'Loading...' : 'Call Lambda'}</button>
-				<br />
-				<span>{msg}</span>
+				<Textarea
+					// initialValue={`
+					// Type some *markdown* here!
+					// <!-- AUTO-GENERATED-CONTENT:START (TOC) -->
+					// toc will be generated here
+					// <!-- AUTO-GENERATED-CONTENT:END -->
+					// # hello world
+
+					// ## good morning
+
+					// ## good afternoon
+
+					// ## good night
+
+					// `}
+
+					value={raw}
+					minRows={3}
+					maxRows={6}
+					buttonText="Magic!"
+					onChange={this.handleRaw}
+					value=""
+				/>
+				<p>{text}</p>
+				<button onClick={this.convertRaw}> lskjd</button>
 			</p>
 		);
 	}
@@ -46,10 +72,10 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<header className="App-header">
+				{/* <header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
 					<h1 className="App-title">Welcome to React</h1>
-				</header>
+				</header> */}
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
 				</p>
--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,7 +1,7 @@
-const leftpad = require('left-pad');
+// const mdmkdn = require('markdown-magic-hosted');
 
 export function handler(event, context, callback) {
-	console.log(event);
+	console.log(event.body);
 	callback(null, {
 		statusCode: 200,
 		body: JSON.stringify({ msg: leftpad('Hello, World!', 50) })
