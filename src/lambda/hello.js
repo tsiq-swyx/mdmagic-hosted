7db0f836f123b652d251457c93821e9a2f85ff0f
tsiq-swyx <shawn.wang@twosigmaiq.com>
2018-07-12 15:17:40 -0400

    try leftpad

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,7 +1,9 @@
+const leftpad = require('left-pad');
+
 export function handler(event, context, callback) {
-  console.log(event)
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({msg: "Hello, World!"})
-  })
+	console.log(event);
+	callback(null, {
+		statusCode: 200,
+		body: JSON.stringify({ msg: leftpad('Hello, World!', 50) })
+	});
 }
